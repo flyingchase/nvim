aee9583e119152d24b47c27f7e8e46d1a7417d1e
wlzhou <zhongbingwang@yeah.net>
2023-02-19 12:56:17 +0800

    [CHANGE]: change default leaderMapping for which-key plugin

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -73,8 +73,8 @@ end
 
 local leader_map = function()
 	vim.g.mapleader = ";"
-	vim.api.nvim_set_keymap("n", ";", "", { noremap = true })
-	vim.api.nvim_set_keymap("x", ";", "", { noremap = true })
+	vim.api.nvim_set_keymap("n", " ", "", { noremap = true })
+	vim.api.nvim_set_keymap("x", " ", "", { noremap = true })
 end
 
 local neovide_config = function()
